b855ea10a432007a034deefd37f3c236222e4756
Thomas <thomas@lekoala.be>
2016-04-21 11:30:59 +0200

    autoconfigure everything from _config

--- _config.php
+++ _config.php
@@ -1,8 +1,18 @@
 <?php
-// Autosetup if key is defined in _ss_environment or yml
+// Autosetup constants defined in _ss_environment
 if (defined('SPARKPOST_API_KEY') && SPARKPOST_API_KEY !== '') {
     SparkPostMailer::config()->api_key = SPARKPOST_API_KEY;
 }
+if (defined('SPARKPOST_SENDING_DISABLED')) {
+    SparkPostMailer::config()->disable_sending = SPARKPOST_SENDING_DISABLED;
+}
+if (defined('SPARKPOST_ENABLE_LOGGING')) {
+    SparkPostMailer::config()->enable_logging = SPARKPOST_ENABLE_LOGGING;
+}
+if (defined('SPARKPOST_SUBACCOUNT_ID')) {
+    SparkPostMailer::config()->subaccount_id = SPARKPOST_SUBACCOUNT_ID;
+}
+// Register as mailer if api key is set
 if (SparkPostMailer::config()->api_key) {
     SparkPostMailer::setAsMailer();
 }
