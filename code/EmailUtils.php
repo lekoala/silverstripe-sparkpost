31930af20d45882c461766fa4874f067b9b5fcf7
Thomas <thomas@lekoala.be>
2021-01-20 09:16:41 +0100

    update emails utils to support emogrifier v3

--- code/EmailUtils.php
+++ code/EmailUtils.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace LeKoala\SparkPost;
 
 use Pelago\Emogrifier;
 
 class EmailUtils
 {
-
     /**
      * Inline styles using Pelago Emogrifier
      *
@@ -16,15 +16,37 @@ class EmailUtils
      */
     public static function inline_styles($html)
     {
-        $emogrifier = new Emogrifier();
-        $emogrifier->setHtml($html);
-        $emogrifier->disableInvisibleNodeRemoval();
-        $emogrifier->enableCssToHtmlMapping();
-        $html = $emogrifier->emogrify();
+        if (class_exists(CssInliner::class)) {
+            // V3
+            $cssInliner = CssInliner::fromHtml($html)->inlineCss('');
+            $domDocument = $cssInliner->getDomDocument();
+
+            // potentially, we could also remove classes
+            // HtmlPruner::fromDomDocument($domDocument)->removeElementsWithDisplayNone()
+            //     ->removeRedundantClassesAfterCssInlined($cssInliner);
+
+            // disableInvisibleNodeRemoval
+            $doc =   HtmlPruner::fromDomDocument($domDocument);
+            if (method_exists($doc, 'removeInvisibleNodes')) {
+                $doc->removeInvisibleNodes();
+            } else {
+                $doc->removeElementsWithDisplayNone();
+            }
+
+            // enableCssToHtmlMapping
+            $html = CssToAttributeConverter::fromDomDocument($domDocument)
+                ->convertCssToVisualAttributes()->render();
+        } else {
+            // V2
+            $emogrifier = new Emogrifier();
+            $emogrifier->disableInvisibleNodeRemoval();
+            $emogrifier->enableCssToHtmlMapping();
+            $emogrifier->setHtml($html);
+            $html = $emogrifier->emogrify();
+        }
 
         return $html;
     }
-
 
     /**
      * Convert an html email to a text email while keeping formatting and links
@@ -42,6 +64,8 @@ class EmailUtils
         $content = str_ireplace(['<strong>', '</strong>', '<b>', '</b>'], "*", $content);
         // Replace links to keep them accessible
         $content = preg_replace('/<a[\s\S]href="(.*?)"[\s\S]*?>(.*?)<\/a>/i', '$2 ($1)', $content);
+        // Replace new lines
+        $content = str_replace(['<br>', '<br/>', '<br />'], "\r\n", $content);
         // Remove html tags
         $content = strip_tags($content);
         // Avoid lots of spaces
