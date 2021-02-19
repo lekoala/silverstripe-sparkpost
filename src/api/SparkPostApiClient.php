a1156dd04e7b85660ed1e1683f23ec7c57b3d9c5
Thomas <thomas@lekoala.be>
2021-02-19 22:26:24 +0100

    remove json last error

--- src/api/SparkPostApiClient.php
+++ src/api/SparkPostApiClient.php
@@ -1060,7 +1060,7 @@ class SparkPostApiClient
         } else {
             $decodedResult = json_decode($result, true);
             if (!$decodedResult) {
-                throw new Exception("Failed to decode $result : " . self::json_last_error_msg());
+                throw new Exception("Failed to decode $result : " . json_last_error_msg());
             }
         }
 
@@ -1136,26 +1136,5 @@ class SparkPostApiClient
     public function getLastResult()
     {
         return end($this->results);
-    }
-
-    /**
-     * Helper for json errors
-     *
-     * @staticvar array $ERRORS
-     * @return string
-     */
-    protected static function json_last_error_msg()
-    {
-        static $ERRORS = [
-            JSON_ERROR_NONE => 'No error',
-            JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
-            JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
-            JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
-            JSON_ERROR_SYNTAX => 'Syntax error',
-            JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
-        ];
-
-        $error = json_last_error();
-        return isset($ERRORS[$error]) ? $ERRORS[$error] : 'Unknown error';
     }
 }
